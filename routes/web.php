07a33b28205334f282501ed2a6fcee77331806b1
bryanHA95 <bryan.huaricapcha.arias@gmail.com>
2021-06-16 23:48:10 -0500

    Actualizacion

--- routes/web.php
+++ routes/web.php
@@ -17,29 +17,3 @@ Route::get('/rubros/{id}', [CategoryController::class,'show'])->name('rubro.show
 
 Route::get('/empresas', [BusinessController::class,'index'])->name('empresa.index');
 Route::get('/empresas/{id}', [BusinessController::class,'show'])->name('empresa.show');
-
-/*
-Route::get('/', [HomeController::class,'index'])->name("home");
-
-Route::post('/register',[BusinessController::class,'store'])->name('bussines.register');
-
-Route::get('/new-user/{id}', [UserController::class,'create'])->name("user.create");
-Route::post('/new-user/store', [UserController::class,'store'])->name("user.store");
-
-Route::get('/ciudades', [CityController::class,'index'])->name('ciudad.index');
-Route::get('/ciudades/{id}', [CityController::class,'show'])->name('ciudad.show');
-
-Route::get('/empresa/{id}', [BusinessController::class,'show'])->name('empresa.show');
-
-Route::get('rubrounico', function(){
-    return view('rubrounico');
-})->name('rubrounico');
-
-Route::get('busqueda_avanzada', function(){
-    return view('busqueda_avanzada');
-})->name('busqueda_avanzada');
-
-Route::get('rubros', function(){
-    return view('rubros');
-})->name('rubros');
-*/
