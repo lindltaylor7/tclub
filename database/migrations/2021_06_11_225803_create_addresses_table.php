3728af48f0a6301fc2e536554abb3291970e1b6b
Jair <riuyagami@gmail.com>
2021-06-17 11:50:06 -0500

    Migrations

---
+++ database/migrations/2021_06_11_225803_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street',50);
+            $table->string('number',10);
+            $table->string('district',50);
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('business_id');
+            $table->foreign('city_id')->references('id')->on('cities')->onDelete('cascade');
+            $table->foreign('business_id')->references('id')->on('businesses')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
